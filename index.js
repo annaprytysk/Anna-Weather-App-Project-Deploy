b6ff63e3d1c8e102dbf51462d0d06e0ade309dba
annaprytysk <anna.prytysk@gmail.com>
2022-12-14 17:39:22 +0100

    Full Forecast - No fake data

--- index.js
+++ index.js
@@ -27,6 +27,70 @@ let daytime = document.querySelector("#currentday");
 let currentTime = new Date();
 daytime.innerHTML = formatDate(currentTime);
 
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 3) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+      <div class="card text-dark bg-info mb-3" style="max-width: 18rem;">
+
+        <div class="card-header" class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <div class="card-body">
+        <h5 class="card-title">
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        </h5>
+        <div class="weather-forecast-temperatures">
+        <p class="card-text">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+      )}° </span>
+          </p>
+        </div>
+        </div>
+      </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "7ed26a6948c661d05fafe7355b41b2ec";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+
+
+
 function displayWeatherCondition(response) {
   celsiusTemp = response.data.main.temp
 
@@ -39,6 +103,8 @@ function displayWeatherCondition(response) {
     response.data.main.humidity;
    document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
 document.querySelector("#weathericon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  getForecast(response.data.coord);
+
 }
 
 function searchCity(city) {
@@ -53,26 +119,8 @@ function handleSubmit(event) {
   searchCity(city);
 }
 
-let celsiusTemp = null;
-
-
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
-
-function displayFahrenheitTemp(event) {
-  event.preventDefault();
-  let FahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    FahrenheitTemp);  
-};
-
-function displayCelsiusTemp(event) { 
-  event.preventDefault();
-    let FahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    FahrenheitTemp);  
-
-};
 
 searchCity("Bratislava");
